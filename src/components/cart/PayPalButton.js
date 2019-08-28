7a6e81b161ec6c45a043d8c5bde8e14985bef0f2
Lyndon Farr <lyndonfarr@protonmail.com>
2019-08-28 14:32:09 +0200

    finished

--- src/components/cart/PayPalButton.js
+++ src/components/cart/PayPalButton.js
@@ -7,7 +7,6 @@ export default class MyApp extends React.Component {
             // Congratulation, it came here means everything's fine!
             console.log("The payment was succeeded!", payment);
             this.props.clearCart();
-            console.log(this.props)
             this.props.history.push('/');
             // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
         }
