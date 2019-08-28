71f581ad02e11b7c69712e60c0a9a70f19cbc760
Lyndon Farr <lyndonfarr@protonmail.com>
2019-08-28 14:34:05 +0200

    finisehd 2

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Title from '../Title';
 import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
