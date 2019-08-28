4a98447e0c803b380e67a98bc69e1ef1e606287f
Lyndon Farr <lyndonfarr@protonmail.com>
2019-08-28 14:27:46 +0200

    history fix

--- src/components/cart/CartTotals.js
+++ src/components/cart/CartTotals.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import PayPalButton from './PayPalButton';
 
-export default function CartTotals({value}) {
-    const {cartSubtotal, cartTax, cartTotal, clearCart, history} = value
+export default function CartTotals({value, history}) {
+    const {cartSubtotal, cartTax, cartTotal, clearCart} = value
     return (
         <React.Fragment>
             <div className="container">
